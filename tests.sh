a1d9496833022044ccc835065957d86df5ce52a4
Ana Paula Lima <anapauladosreislima@gmail.com>
2015-09-02 14:57:01 -0300

    Muda shell script para rodar thread

--- tests.sh
+++ tests.sh
@@ -10,14 +10,14 @@ do
 	# ten times to have an average
 	for((i=0 ;i<10;i++))
 	do
-		(/usr/bin/time -f "\n%e" ./sequencial < $test_file) 2> tmp-$test_file 1> result-$test_file
+		(/usr/bin/time -f "\n%e" ./paralelo < $test_file) 2> tmp-$test_file 1> result-$test_file
 		total_time_single=$(python -c "print $total_time_single + $(tail -n 1 tmp-$test_file)")
 
 
 	done
 
 	echo $test_file
-	echo Average Singlethreaded Time: $(python -c "print \"%.3f\" % ($total_time_single/10)")
+	echo Average Paralel Time: $(python -c "print \"%.3f\" % ($total_time_single/10)")
 	echo 
 
 	rm tmp-$test_file
