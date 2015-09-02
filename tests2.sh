6ec5a146798fd6da5e165ea8426423026f07b8cc
Ana Paula Lima <anapauladosreislima@gmail.com>
2015-09-02 15:46:07 -0300

    Arruma script

--- tests2.sh
+++ tests2.sh
@@ -1,18 +1,18 @@
 #! /bin/bash
 
 # for each matrix file
-for test_file in $(ls matrizes/)
-do
+ test_file=matriz1000.txt
+
 
 
 
 	# ten times to have an average
-	for((j=0 ;j<20;j++))
+	for((j=1;j<1000;j+=100))
 	do
 		total_time_single=0
 		for((i=0 ;i<2;i++))
 		do
-			(/usr/bin/time -f "\n%e" ./paralelo $j < $test_file) 2> tmp-$test_file 1> result-$test_file
+			(/usr/bin/time -f "\n%e" ./paralelo $j < matrizes/$test_file) 2> tmp-$test_file 1> result-$test_file
 			total_time_single=$(python -c "print $total_time_single + $(tail -n 1 tmp-$test_file)")
 
 
@@ -25,4 +25,3 @@ do
 	done
 
 	rm tmp-$test_file
-done
