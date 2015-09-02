08a19a565f8095d5ee6dcb7e2146b3bc9e3e3e0a
Ana Paula Lima <anapauladosreislima@gmail.com>
2015-09-01 22:33:01 -0300

    Inclui shell script

---
+++ tests.sh
@@ -0,0 +1,24 @@
+#! /bin/bash
+
+# for each matrix file
+for test_file in $(ls -p | grep matriz | grep -v /)
+do
+
+	total_time_single=0
+	total_time_multi=0
+
+	# ten times to have an average
+	for((i=0 ;i<10;i++))
+	do
+		(/usr/bin/time -f "\n%e" ./sequencial < $test_file) 2> tmp-$test_file 1> result-$test_file
+		total_time_single=$(python -c "print $total_time_single + $(tail -n 1 tmp-$test_file)")
+
+
+	done
+
+	echo $test_file
+	echo Average Singlethreaded Time: $(python -c "print \"%.3f\" % ($total_time_single/10)")
+	echo 
+
+	rm tmp-$test_file
+done
